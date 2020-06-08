6871855dabaca1b4d3b0bb3a9389968510ee5b71
Hanna Kozak <hannawkozak@gmail.com>
2020-06-08 16:01:19 +0100

    Updates

---
+++ precache-manifest.36bcc6edf3ce3bb3700dcbd2eac195b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec48958ea50d0f8b37575a2b4c19bf2",
+    "url": "/would-you/index.html"
+  },
+  {
+    "revision": "52fbca0dee1586349b24",
+    "url": "/would-you/static/css/main.61c23fe8.chunk.css"
+  },
+  {
+    "revision": "c52cc6ad51709b6c5b26",
+    "url": "/would-you/static/js/2.d3ac44c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/would-you/static/js/2.d3ac44c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fbca0dee1586349b24",
+    "url": "/would-you/static/js/main.ae4fa6f9.chunk.js"
+  },
+  {
+    "revision": "c87f926ff1cc1f1b38e8",
+    "url": "/would-you/static/js/runtime-main.6ffe0cc0.js"
+  }
+]);
